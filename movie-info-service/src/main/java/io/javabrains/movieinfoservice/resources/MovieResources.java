2e975c35324042781052e1eea89e48dbce28c73e
Pydare <dareyadewumi650@gmail.com>
2020-06-07 18:25:21 +0100

    created skeletons for the 3 microframeworks

---
+++ movie-info-service/src/main/java/io/javabrains/movieinfoservice/resources/MovieResources.java
@@ -0,0 +1,18 @@
+package io.javabrains.movieinfoservice.resources;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.javabrains.movieinfoservice.models.Movie;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieResources {
+
+    @RequestMapping("/{movieId}")
+    public Movie getMovieInfo(@PathVariable String movieId){
+        return new Movie(movieId,"Test name"); 
+    }
+
+}
